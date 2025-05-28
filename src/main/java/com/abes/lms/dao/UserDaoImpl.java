561f1e4cd3df6c8fe3966af0854ccf795679c58a
Divyansh Pandey <pandey.divyansh321@gamil.com>
2025-05-28 16:14:23 +0530

    UserDao

--- src/main/java/com/abes/lms/dao/UserDaoImpl.java
+++ src/main/java/com/abes/lms/dao/UserDaoImpl.java
@@ -1,7 +1,21 @@
+package com.abes.lms.dao;
 
+import com.abes.lms.dto.UserDto;
+import com.abes.lms.exception.UserNotFoundException;
+import com.abes.lms.util.CollectionUtil;
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
 
-   
+    @Override
+    public List<UserDto> getAllUsers() {
+        return CollectionUtil.getAllUsers();
+    }
+
+    @Override
+    public void addUser(UserDto user) {
+        CollectionUtil.addUser(user);
+    }
+
+  
 }
