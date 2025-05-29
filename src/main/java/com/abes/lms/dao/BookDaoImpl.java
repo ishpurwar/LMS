84a09f04894b34525fa924a5bea7ce7fa34b78b6
Ish Purwar <96423154+ishpurwar@users.noreply.github.com>
2025-05-30 03:39:14 +0530

    Implementation of BookDaoImpl and fixes

--- src/main/java/com/abes/lms/dao/BookDaoImpl.java
+++ src/main/java/com/abes/lms/dao/BookDaoImpl.java
@@ -1,7 +1,31 @@
 package com.abes.lms.dao;
 
+import com.abes.lms.dto.BookDTO;
+import com.abes.lms.exception.BookNotFoundException;
+import com.abes.lms.util.CollectionUtil;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookDaoImpl implements BookDao {
 
- 
+    @Override
+    public List<BookDTO> getAllBooks() {
+        return CollectionUtil.getAllBooks();
+    }
+
+    @Override
+    public void addBook(BookDTO book) {
+        CollectionUtil.addBook(book);
+    }
+
+    @Override
+    public boolean removeBook(String title) throws BookNotFoundException {
+        if (!CollectionUtil.isBookPresent(title)) {
+            throw new BookNotFoundException("Book '" + title + "' not found");
+        }
+        return CollectionUtil.removeBook(title);
+    }
+
+  
 }
