f10370df0cb32c08e55f4cf5ba745ce888441583
TSS-sniper <tarunsinghsheoran9703@gmail.com>
2025-05-29 16:01:46 +0530

    exception changes

--- src/main/java/com/abes/lms/exception/BookNotFoundException.java
+++ src/main/java/com/abes/lms/exception/BookNotFoundException.java
@@ -1,12 +1,6 @@
 package com.abes.lms.exception;
 
-<<<<<<< HEAD
 public class BookNotFoundException extends Exception {
 	public BookNotFoundException(String message) {
 		super(message);
-	}
-=======
-public class BookNotFoundException {
-    
->>>>>>> main
-}
+	}}
--- src/main/java/com/abes/lms/exception/InputValidationException.java
+++ src/main/java/com/abes/lms/exception/InputValidationException.java
@@ -1,13 +1,8 @@
 package com.abes.lms.exception;
 
-<<<<<<< HEAD
 public class InputValidationException extends Exception {
 	public InputValidationException(String message) {
 		super(message);
 	}
 }
-=======
-public class InputValidationException {
-    
-}
->>>>>>> main
+
