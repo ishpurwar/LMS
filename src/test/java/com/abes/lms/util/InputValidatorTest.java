f8bdb6f5107178ce17be2c80924ae45ff48d20b4
Divyansh Pandey <97462241+divyanshhpandey@users.noreply.github.com>
2025-05-30 15:47:03 +0530

    InputValidatorTest Added

--- src/test/java/com/abes/lms/util/InputValidatorTest.java
+++ src/test/java/com/abes/lms/util/InputValidatorTest.java
@@ -1,5 +1,26 @@
 package com.abes.lms.util;
 
+import com.abes.lms.exception.InputValidationException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class InputValidatorTest {
     
+    @Test
+    void testValidateBookTitle_Valid() {
+        assertDoesNotThrow(() -> InputValidator.validateBookTitle("Valid Title"));
+        assertDoesNotThrow(() -> InputValidator.validateBookTitle("A"));
+        assertDoesNotThrow(() -> InputValidator.validateBookTitle("A".repeat(100)));
+    }
+    
+    @Test
+    void testValidateBookTitle_Null() {
+        InputValidationException exception = assertThrows(
+            InputValidationException.class,
+            () -> InputValidator.validateBookTitle(null)
+        );
+        assertEquals("Book title cannot be empty", exception.getMessage());
+    }
+    
+   
 }
