5089e220476574c44d127c50bb74b02c0efcedf4
Ish Purwar <96423154+ishpurwar@users.noreply.github.com>
2025-05-30 07:13:06 +0530

    user services implementation get book and sort and user detatils

--- src/main/java/com/abes/lms/service/UserService.java
+++ src/main/java/com/abes/lms/service/UserService.java
@@ -1,5 +1,38 @@
 package com.abes.lms.service;
 
+import java.util.List;
+
+import com.abes.lms.dao.BookDao;
+import com.abes.lms.dao.BookDaoImpl;
+import com.abes.lms.dao.UserDao;
+import com.abes.lms.dao.UserDaoImpl;
+import com.abes.lms.dto.BookDTO;
+import com.abes.lms.dto.UserDto;
+import com.abes.lms.exception.UserNotFoundException;
+
 public class UserService {
-    
+    private BookDao bookDao;
+    private UserDao userDao;
+    public UserService() {
+        this.bookDao = new BookDaoImpl();
+        this.userDao = new UserDaoImpl();
+    }
+    public List<BookDTO> getAllBooks() {
+        return bookDao.getAllBooks();
+    }
+      public List<BookDTO> sortBooksById() {
+        return bookDao.sortBooksById();
+    }
+
+    public List<BookDTO> sortBooksByRating() {
+        return bookDao.sortBooksByRating();
+    }
+
+    public List<BookDTO> sortBooksByTitle() {
+        return bookDao.sortBooksByTitle();
+    }
+
+    public UserDto getUserDetails(String userName) throws UserNotFoundException {
+        return userDao.findUserByName(userName);
+    }
 }
