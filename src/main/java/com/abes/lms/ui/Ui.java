525e622fa1be18bd4a59c84fa0ce80583a7bdde1
Ish Purwar <96423154+ishpurwar@users.noreply.github.com>
2025-05-31 14:30:19 +0530

    exception

--- src/main/java/com/abes/lms/ui/Ui.java
+++ src/main/java/com/abes/lms/ui/Ui.java
@@ -2,8 +2,11 @@ package com.abes.lms.ui;
 
 import com.abes.lms.dto.BookDTO;
 import com.abes.lms.dto.UserDto;
+import com.abes.lms.exception.BookAlreadyBorrowException;
+import com.abes.lms.exception.BookAlreadyExistException;
 import com.abes.lms.exception.BookNotFoundException;
 import com.abes.lms.exception.InputValidationException;
+import com.abes.lms.exception.UserAlreadyExistException;
 import com.abes.lms.exception.UserNotFoundException;
 import com.abes.lms.service.LibrarianService;
 import com.abes.lms.service.UserService;
@@ -76,6 +79,12 @@ public class Ui {
 			System.out.println("Registration successful!");
 		} catch (InputValidationException e) {
 			System.out.println("Registration failed: " + e.getMessage());
+		}
+		catch (UserAlreadyExistException e){
+			System.out.println("Registration failed: " + e.getMessage());
+		}
+		catch (Exception e) {
+			System.out.println("An unexpected error occurred: " + e.getMessage());
 		}
 	}
 
@@ -221,7 +230,7 @@ public class Ui {
 		try {
 			userService.borrowBook(currentUser, title);
 			System.out.println("Book borrowed successfully!");
-		} catch (BookNotFoundException | UserNotFoundException | InputValidationException e) {
+		} catch (BookNotFoundException | UserNotFoundException | InputValidationException | BookAlreadyBorrowException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 	}
@@ -233,7 +242,7 @@ public class Ui {
 		try {
 			userService.returnBook(currentUser, title);
 			System.out.println("Book returned successfully!");
-		} catch (BookNotFoundException | UserNotFoundException | InputValidationException e) {
+		} catch (BookNotFoundException | UserNotFoundException | InputValidationException | BookAlreadyExistException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 	}
@@ -252,7 +261,7 @@ public class Ui {
 				currentUser = newName;
 			}
 			System.out.println("Profile updated successfully!");
-		} catch (UserNotFoundException | InputValidationException e) {
+		} catch (UserNotFoundException | InputValidationException | UserAlreadyExistException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 	}
@@ -307,6 +316,11 @@ public class Ui {
 			System.out.println("Invalid rating format. Please enter a decimal number.");
 		} catch (InputValidationException e) {
 			System.out.println("Error: " + e.getMessage());
+		}
+		catch (BookAlreadyExistException e) {
+			System.out.println("Error: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("An unexpected error occurred: " + e.getMessage());
 		}
 	}
 
